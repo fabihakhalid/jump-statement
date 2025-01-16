06e126ed027a2ff4baf21e63db10be86c4630e3b
fabiha khalid <fabihakhalid2005@gmail.com>
2025-01-16 11:12:39 -0800

    done by fabiha

---
+++ f even odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int checkEvenodd(int num) {
+	if(num%2==0)
+	return 1;
+	else 
+	return 0;
+	
+}
+int main(){
+	int num =6;
+	if(checkEvenodd(num))
+	cout<<"num"<<" is even" <<endl;
+	else 
+	cout <<"num "<<" is odd" <<endl;
+	return 0;
+	
+}
