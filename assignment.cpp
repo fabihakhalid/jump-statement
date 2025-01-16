a054a4142b4163505c49def60cefabe8c86b866e
fabiha khalid <fabihakhalid2005@gmail.com>
2025-01-16 11:05:51 -0800

    done by fabiha

---
+++ assignment.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string correctPassword ="12345";
+	string enteredPassword;
+	int attempts = 0;
+	start :
+		if (attempts >=3){
+			cout<<"too many incorrect password "<<endl;
+			return 1;
+			
+		}
+		cout<<"enter password : ";
+		cin>>enteredPassword;
+		
+		if (enteredPassword==correctPassword){
+			cout<<"wellcome"<<endl;
+			return 0;
+		}else{
+			attempts++;
+			cout<<"incorrect.attempets left"<<3-attempts<<endl;
+			goto start;
+		}
+		return 0;
+}
